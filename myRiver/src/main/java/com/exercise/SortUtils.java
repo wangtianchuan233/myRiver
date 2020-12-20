915adf4cd846fffa6d264e518f8636c92d06d332
wangtianchuan233 <1059856744@qq.com>
2020-12-20 21:06:23 +0800

    Method quickSort was added.

--- myRiver/src/main/java/com/exercise/SortUtils.java
+++ myRiver/src/main/java/com/exercise/SortUtils.java
@@ -5,5 +5,30 @@ package com.exercise;
  * 2020/12/20 0020
  */
 public class SortUtils {
+    public static <T extends Comparable> void quickSort(T[] array,int left,int right){
+        int i = left, j = right;
+        T middle = array[(i + j) / 2];
 
+        do{
+            while(array[i].compareTo(middle) < 0 && i < right){
+                i++;
+            }
+            while(array[j].compareTo(middle) > 0 && j > left){
+                j--;
+            }
+            if (i <= j){
+                T tem = array[i];
+                array[i] = array[j];
+                array[j] = tem;
+                i++;
+                j--;
+            }
+        }while(i < j);
+        if (i < right){
+            quickSort(array,i,right);
+        }
+        if (j > left){
+            quickSort(array,left,j);
+        }
+    }
 }

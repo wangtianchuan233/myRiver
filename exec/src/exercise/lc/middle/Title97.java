10cd8108bbb43640bb5c6fec7f5c230e7091e8f2
wangtianchuan <1059856744@qq.com>
2021-06-28 12:48:59 +0800

    2021-6-23 10:38:15

--- exec/src/exercise/lc/middle/Title97.java
+++ exec/src/exercise/lc/middle/Title97.java
@@ -58,26 +58,7 @@ public class Title97 {
         }
         return b[m][n];
     }
-    /**
-     * 滚动数组优化空间复杂度
-     */
-    public static boolean isInterleave4(String s1, String s2, String s3){
-        int m = s1.length(), n = s2.length();
-        if (m + n != s3.length()){
-            return false;
-        }
-        boolean[] b = new boolean[n + 1];
-        b[0] = true;
-        for(int j = 1; j < n + 1; j++){
-            b[j] = b[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
-        }
-        for(int i = 1; i < m + 1; i++){
-            for(int j = 1; j < n + 1; j++){
-                b[j] = (b[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1)) || (b[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1));
-            }
-        }
-        return b[n];
-    }
+
     /**
      * 超出时间限制
      */
